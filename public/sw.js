5c180efb21a9cc6ab10013f67174a2d70d687326
RitaYY1129 <RitaYY1129@users.noreply.github.com>
2026-08-12 15:47:59 +0800

    修复 iPhone 主屏幕应用启动地址

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = 'love-diary-v3'
+const CACHE_NAME = 'love-diary-v5'
 const BASE_URL = new URL(self.registration.scope).pathname
 const appUrl = path => `${BASE_URL}${path}`
 const APP_SHELL = [
   appUrl(''),
+  appUrl('index.html'),
   appUrl('manifest.webmanifest'),
   appUrl('assets/img/icon-192.png'),
   appUrl('assets/img/icon-512.png')
@@ -35,7 +36,7 @@ self.addEventListener('fetch', event => {
           caches.open(CACHE_NAME).then(cache => cache.put(appUrl(''), copy))
           return response
         })
-        .catch(() => caches.match(appUrl('')))
+        .catch(() => caches.match(appUrl('index.html')).then(response => response || caches.match(appUrl(''))))
     )
     return
   }
